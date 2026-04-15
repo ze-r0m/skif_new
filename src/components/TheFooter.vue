679ed6e804ca1035acda79460c294f1d3dedcf6f
ze-r0m <rzv2003@gmail.com>
2026-04-15 12:37:20 +0300

    feat: мобильная адаптация футера

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -12,14 +12,24 @@
           </a>
           </div>
           <div class="footer__links">
-            <a class="footer__link text-h4-desktop" href="https://do.skif.donstu.ru/" target="_blank">ДО.СКИФ</a>
+            <a class="footer__link text-h4-desktop" href="https://do.skif.donstu.ru/" target="_blank">ДО.СКИФ
+              <IconLinkExternal class="footer__link-icon" />
+            </a>
             <a class="footer__link text-h4-desktop" href="https://skif.donstu.ru/spec/" target="_blank">СКИФ.СПЕЦ</a>
-            <a class="footer__link text-h4-desktop" href="https://donstu.ru/" target="_blank">Главный сайт</a>
+            <a class="footer__link text-h4-desktop" href="https://donstu.ru/" target="_blank">Главный сайт
+              <IconLinkExternal class="footer__link-icon" />
+            </a>
             <a class="footer__link text-h4-desktop" href="https://skif.donstu.ru/test/" target="_blank">СКИФ.ТЕСТ</a>
-            <a class="footer__link text-h4-desktop" href="https://int.skif.donstu.ru/" target="_blank">СКИФ.Международный</a>
+            <a class="footer__link text-h4-desktop" href="https://int.skif.donstu.ru/" target="_blank">СКИФ.Международный
+              <IconLinkExternal class="footer__link-icon" />
+            </a>
             <a class="footer__link text-h4-desktop" href="https://skif.donstu.ru/old" target="_blank">Старая версия сайта</a>
-            <a class="footer__link text-h4-desktop" href="https://de.donstu.ru/zaoch/organizations/1" target="_blank">СКИФ.Библиотека</a>
-            <a class="footer__link text-h4-desktop" href="https://prof.skif.donstu.ru/" target="_blank">ПРОФ.СКИФ</a>
+            <a class="footer__link text-h4-desktop" href="https://de.donstu.ru/zaoch/organizations/1" target="_blank">СКИФ.Библиотека
+              <IconLinkExternal class="footer__link-icon" />
+            </a>
+            <a class="footer__link text-h4-desktop" href="https://prof.skif.donstu.ru/" target="_blank">ПРОФ.СКИФ
+              <IconLinkExternal class="footer__link-icon" />
+            </a>
 
           </div>
         </div>
@@ -30,39 +40,27 @@
             </div>
           </div>
           <div class="footer__contact-items">
+
             <div class="footer__contact-item">
-              <p class="text-body-small-bold">Приёмная ректора</p>
+              <p class="text-body-bold-desktop">Управление цифровых образовательных технологий</p>
               <time class="text-body-small">Пн. - Пт.: 8:30 - 17:00</time>
-              <a class="footer__contact-link text-body-small" href="tel:88632738525">8 863 273 85 25</a>
-              <address class="text-body-small">344003, г. Ростов-на-Дону, пл. Гагарина, 1, ауд. 250</address>
-              <a class="footer__contact-link text-body-small" href="mailto:reception@donstu.ru">reception@donstu.ru</a>
-            </div>
-            <div class="footer__contact-item">
-              <p class="text-body-small-bold">Приёмная комиссия</p>
-              <time class="text-body-small">Пн. - Пт.: 8:30 - 17:00</time>
-              <a class="footer__contact-link text-body-small" href="tel:88001001930">8 800 100 19 30</a>
-              <address class="text-body-small">344003, г. Ростов-на-Дону, пл. Гагарина, 1, главный корпус, ауд. 143а, 102</address>
-              <a class="footer__contact-link text-body-small" href="mailto:spu-33@donstu.ru">spu-33@donstu.ru</a>
-            </div>
-            <div class="footer__contact-item">
-              <p class="text-body-small-bold">Пресс-служба</p>
-              <time class="text-body-small">Пн. - Пт.: 8:30 - 17:00</time>
-              <a class="footer__contact-link text-body-small" href="tel:88632381700">8 863 238 17 00</a>
-              <address class="text-body-small">344003, г. Ростов-на-Дону, пл. Гагарина, 1, ауд. 7-329</address>
-              <a class="footer__contact-link text-body-small" href="mailto:isdstu@mail.ru">isdstu@mail.ru</a>
-            </div>
-            <div class="footer__contact-item">
-              <p class="text-body-small-bold">Начальник УЦОТ</p>
-              <p class="text-body-small footer__person">Шлыкова Алла Ивановна</p>
-              <a class="footer__contact-link text-body-small" href="tel:+78632381583">+7 (863) 238-15-83 (внутр. 3583)</a>
-              <address class="text-body-small">г. Ростов-на-Дону, пл. Гагарина, 1, ауд. 1-420а</address>
-            </div>
-            <div class="footer__contact-item">
-              <p class="text-body-small-bold">Отдел сопровождения дистанционного образования</p>
-              <p class="text-body-small footer__person">Начальник отдела — Ачаров Борис Фёдорович</p>
-              <a class="footer__contact-link text-body-small" href="tel:+78632738473">+7 (863) 273-84-73 (внутр. 2473)</a>
-              <address class="text-body-small">г. Ростов-на-Дону, пл. Гагарина, 1, ауд. 1-423</address>
+              <a class="footer__contact-link text-body-small" href="tel:88632381583">8 863 238 15 83</a>
+              <address class="text-body-small">344003, г. Ростов-на-Дону, пл. Гагарина, 1, ауд. 1-420а</address>
               <a class="footer__contact-link text-body-small" href="mailto:spu-06.1@donstu.ru">spu-06.1@donstu.ru</a>
+            </div>
+            <div class="footer__contact-item">
+              <p class="text-body-bold-desktop">Отдел сопровождения дистанционного образования</p>
+              <time class="text-body-small">Пн. - Пт.: 8:30 - 17:00</time>
+              <a class="footer__contact-link text-body-small" href="tel:88632738473">8 863 273 84 73</a>
+              <address class="text-body-small">344003, г. Ростов-на-Дону, пл. Гагарина, 1, ауд. 1-423</address>
+              <a class="footer__contact-link text-body-small" href="mailto:bacharov@donstu.ru">bacharov@donstu.ru</a>
+            </div>
+            <div class="footer__contact-item">
+              <p class="text-body-bold-desktop">Отдел сопровождения образовательной логистики</p>
+              <time class="text-body-small">Пн. - Пт.: 8:30 - 17:00</time>
+              <a class="footer__contact-link text-body-small" href="tel:88632738441">8 863 273 84 41</a>
+              <address class="text-body-small">344003, г. Ростов-на-Дону, пл. Гагарина, 1, ауд. 1-420</address>
+              <a class="footer__contact-link text-body-small" href="mailto:ognedina@donstu.ru">ognedina@donstu.ru</a>
             </div>
           </div>
         </div>
@@ -81,6 +79,7 @@
 import { computed } from 'vue';
 import IconSkifSquare from "@/components/icons/IconSkifSquare.vue";
 import IconDstuUcotBrandWhite from "@/components/icons/IconDstuUcotBrandWhite.vue";
+import IconLinkExternal from "@/components/icons/IconLinkExternal.vue";
 
 const currentYear = computed(() => new Date().getFullYear());
 </script>
@@ -88,10 +87,13 @@ const currentYear = computed(() => new Date().getFullYear());
 <style scoped>
 /* ==================== Общие ==================== */
 .footer {
-  background-color: #0E468B;
+  background-color: rgba(14, 70, 139, 1);
   color: #ffffff;
 }
 
+.footer__head-contacts {
+  display: none;
+}
 
 /* ==================== footer__head ==================== */
 .footer__head {
@@ -103,13 +105,20 @@ const currentYear = computed(() => new Date().getFullYear());
   padding-bottom: 56px;
 }
 
+.footer__logos {
+  display: flex;
+  gap: 20px;
+  align-items: center;
+}
+
 .footer__logo {
   display: flex;
   align-items: center;
   flex-shrink: 0;
   height: 65px;
 }
-.footer__logos{
+
+.footer__logos {
   gap: 20px;
   display: flex;
   height: 34px;
@@ -136,6 +145,15 @@ const currentYear = computed(() => new Date().getFullYear());
   color: #ffffff;
   text-decoration: none;
   transition: opacity 0.3s ease;
+  display: inline-flex;
+  align-items: center;
+  gap: 12px;
+}
+
+.footer__link-icon {
+  width: 24px;
+  height: 24px;
+  flex-shrink: 0;
 }
 
 .footer__link:hover {
@@ -165,28 +183,22 @@ const currentYear = computed(() => new Date().getFullYear());
 
 .footer__contact-items {
   display: grid;
-  gap: 3.2rem;
+  gap: 32px;
   grid-template-columns: repeat(3, 1fr);
 }
 
 .footer__contact-item {
   display: flex;
   flex-direction: column;
-  gap: 6px;
+  gap: 14px;
 }
 
-.footer__contact-item > .text-body-small-bold {
-  color: #ffffff;
-  margin: 0;
-}
-
-.footer__person {
-  color: rgba(255, 255, 255, 0.7);
+.footer__contact-item > .text-body-bold-desktop {
   margin: 0;
 }
 
 .footer__contact-item time {
-  color: rgba(255, 255, 255, 0.6);
+  color: rgba(255, 255, 255, 0.70);
 }
 
 .footer__contact-link {
@@ -201,7 +213,6 @@ const currentYear = computed(() => new Date().getFullYear());
 
 .footer__contact-item address {
   font-style: normal;
-  color: rgba(255, 255, 255, 0.6);
 }
 
 /* ==================== footer__foot ==================== */
@@ -220,101 +231,153 @@ const currentYear = computed(() => new Date().getFullYear());
 
 .footer__paraweb img {
   height: 20px;
-  width: auto;
-  opacity: 0.6;
+  width: 94px;
+  opacity: 0.4;
   transition: opacity 0.3s ease;
 }
 
-.footer__paraweb:hover img {
-  opacity: 1;
-}
+
 
 /* =======================================================
    Tablet (744px – 1023px)
    ======================================================= */
 @media (min-width: 744px) and (max-width: 1023px) {
-  .footer__bottom {
-    padding-top: 40px;
-  }
-
   .footer__head {
-    grid-template-columns: 1fr;
-    gap: 32px;
+    padding-top: 40px;
     padding-bottom: 32px;
   }
 
+  .footer__logos {
+    height: 56px;
+    width: auto;
+  }
+
+  .footer__logo {
+    height: 56px;
+  }
+
+  .logo-icon--skif {
+    width: 56px;
+    height: 56px;
+  }
+
+  .logo-icon--dstu {
+    width: auto;
+    height: 56px;
+  }
+
   .footer__links {
-    grid-template-columns: repeat(2, 1fr);
-    gap: 20px;
+    display: none;
   }
 
   .footer__contacts {
-    grid-template-columns: 1fr;
+    display: flex;
+    flex-direction: column-reverse;
     gap: 32px;
-    padding: 0 40px ;
+    border-top: none;
+    padding: 0;
   }
 
   .footer__contact-items {
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: repeat(3, 1fr);
     gap: 24px;
   }
+
+  .footer__tagline-wrapper {
+    width: 100%;
+    border-top: 1px solid rgba(210, 218, 227, 0.30);
+    padding: 24px 0;
+  }
+
+  .footer__tagline {
+    height: 26px;
+    width: 260px;
+  }
+
+  .footer__foot {
+    display: flex;
+    padding: 20px 0;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
+    gap: 20px;
+  }
+
+
+
 }
 
 /* =======================================================
    Mobile (до 743px)
    ======================================================= */
 @media (max-width: 743px) {
-  .footer__bottom {
-    padding-top: 32px;
-  }
 
   .footer__head {
-    grid-template-columns: 1fr;
-    gap: 24px;
-    padding-bottom: 24px;
+    padding-top: 40px;
+    padding-bottom: 32px;
   }
 
-  .footer__links {
-    grid-template-columns: 1fr;
-    gap: 12px;
+  .footer__logos {
+    height: 56px;
+    width: auto;
+  }
+
+  .footer__logo {
+    height: 56px;
   }
 
   .logo-icon--skif {
-    width: 48px;
-    height: 48px;
+    width: 56px;
+    height: 56px;
   }
 
   .logo-icon--dstu {
-    height: 32px;
+    width: auto;
+    height: 56px;
+  }
+
+  .footer__links {
+    display: none;
   }
 
   .footer__contacts {
-    grid-template-columns: 1fr;
-    gap: 24px;
-    padding-top: 24px;
-  }
-
-  .footer__tagline {
-    width: 100%;
-    height: auto;
-  }
-
-  .footer__tagline img {
-    height: 28px;
-    width: auto;
+    display: flex;
+    flex-direction: column-reverse;
+    gap: 20px;
+    border-top: none;
+    padding: 0;
   }
 
   .footer__contact-items {
     grid-template-columns: 1fr;
-    gap: 20px;
+    gap: 24px;
+  }
+  .footer__contact-item {
+    gap: 8px;
+  }
+
+  .footer__contact-item > p{
+    font-size: 14px;
+  }
+
+  .footer__tagline-wrapper {
+    width: 100%;
+    border-top: 1px solid rgba(210, 218, 227, 0.30);
+    padding: 32px 0;
+  }
+
+  .footer__tagline {
+    height: 26px;
+    width: 260px;
   }
 
   .footer__foot {
+    display: flex;
+    padding: 20px 0;
     flex-direction: column;
-    gap: 12px;
-    text-align: center;
-    margin-top: 24px;
-    padding: 16px 0;
+    justify-content: space-between;
+    align-items: flex-start;
+    gap: 20px;
   }
 }
 </style>
---
+++ src/components/icons/IconLinkExternal.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M12.7998 11.2006L19.3598 4.64062" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M20.0002 7.84V4H16.1602" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M11.2 4H9.6C5.6 4 4 5.6 4 9.6V14.4C4 18.4 5.6 20 9.6 20H14.4C18.4 20 20 18.4 20 14.4V12.8" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+
+</template>
