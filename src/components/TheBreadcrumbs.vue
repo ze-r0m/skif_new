8670938cc3aea0097cd4f9d24cc7edceba4d66dc
ze-r0m <rzv2003@gmail.com>
2026-04-28 12:05:24 +0300

    fix: mobile breadcrumbs, color icon

--- src/components/TheBreadcrumbs.vue
+++ src/components/TheBreadcrumbs.vue
@@ -1,56 +1,30 @@
 <template>
-  <div class="breadcrumbs-wrapper">
-    <!-- Desktop version -->
-    <ol class="breadcrumbs" itemscope itemtype="https://schema.org/BreadcrumbList">
-      <li
-        v-for="(item, index) in items"
-        :key="index"
-        class="breadcrumbs__item"
-        :class="{ 'breadcrumbs__item--current': !item.to }"
-        itemprop="itemListElement"
-        itemscope
-        itemtype="https://schema.org/ListItem"
+  <ol class="breadcrumbs" itemscope itemtype="https://schema.org/BreadcrumbList">
+    <li
+      v-for="(item, index) in items"
+      :key="index"
+      class="breadcrumbs__item"
+      :class="{ 'breadcrumbs__item--current': !item.to }"
+      itemprop="itemListElement"
+      itemscope
+      itemtype="https://schema.org/ListItem"
+    >
+      <router-link
+        v-if="item.to"
+        class="breadcrumbs__item-link"
+        :to="item.to"
+        itemprop="item"
       >
-        <router-link
-          v-if="item.to"
-          class="breadcrumbs__item-link"
-          :to="item.to"
-          itemprop="item"
-        >
-          <IconHome v-if="index === 0" />
-          <span v-else class="text-body">{{ item.title }}</span>
-          <meta itemprop="position" :content="index + 1">
-        </router-link>
-        <span v-else class="breadcrumbs__item-text text-body">
-          {{ item.title }}
-          <meta itemprop="position" :content="index + 1">
-        </span>
-      </li>
-    </ol>
-    
-    <!-- Mobile version: show only previous page link with back arrow -->
-    <ol v-if="items.length >= 2" class="breadcrumbs breadcrumbs--mobile" itemscope itemtype="https://schema.org/BreadcrumbList">
-      <li
-        class="breadcrumbs__item"
-        itemprop="itemListElement"
-        itemscope
-        itemtype="https://schema.org/ListItem"
-      >
-        <router-link
-          class="breadcrumbs__item-link"
-          :to="items[items.length - 2].to"
-          itemprop="item"
-        >
-          <svg class="breadcrumbs__arrow text-body" width="16" height="16" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M12.2514 28.4213C11.9902 28.4213 11.7289 28.325 11.5227 28.1188C11.1239 27.72 11.1239 27.06 11.5227 26.6613L20.4877 17.6963C21.1477 17.0363 21.1477 15.9638 20.4877 15.3038L11.5227 6.33876C11.1239 5.94001 11.1239 5.28001 11.5227 4.88126C11.9214 4.48251 12.5814 4.48251 12.9802 4.88126L21.9452 13.8463C22.6464 14.5475 23.0452 15.4963 23.0452 16.5C23.0452 17.5038 22.6602 18.4525 21.9452 19.1538L12.9802 28.1188C12.7739 28.3113 12.5127 28.4213 12.2514 28.4213Z" fill="#80889D"/>
-          </svg>
-          <IconHome v-if="items.length === 2" />
-          <span v-else class="breadcrumbs__item-text text-body-small">{{ items[items.length - 2].title }}</span>
-          <meta itemprop="position" :content="items.length - 1">
-        </router-link>
-      </li>
-    </ol>
-  </div>
+        <IconHome v-if="index === 0" />
+        <span v-else class="breadcrumbs__link-text text-body">{{ item.title }}</span>
+        <meta itemprop="position" :content="index + 1">
+      </router-link>
+      <span v-else class="breadcrumbs__item-text text-body">
+        {{ item.title }}
+        <meta itemprop="position" :content="index + 1">
+      </span>
+    </li>
+  </ol>
 </template>
 
 <script setup>
@@ -76,32 +50,30 @@ defineProps({
   flex-wrap: wrap;
 }
 
-ol.breadcrumbs {
-  margin: 0 0 48px;
-}
-
 .breadcrumbs__item {
   display: flex;
   align-items: center;
-  gap: 8px;
+  gap: 16px;
   margin: 0;
   padding: 0;
+
 }
 
 .breadcrumbs__item:not(:last-child)::after {
   content: '';
   width: 16px;
   height: 16px;
-  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 33 33' fill='none'%3E%3Cpath d='M12.2514 28.4213C11.9902 28.4213 11.7289 28.325 11.5227 28.1188C11.1239 27.72 11.1239 27.06 11.5227 26.6613L20.4877 17.6963C21.1477 17.0363 21.1477 15.9638 20.4877 15.3038L11.5227 6.33876C11.1239 5.94001 11.1239 5.28001 11.5227 4.88126C11.9214 4.48251 12.5814 4.48251 12.9802 4.88126L21.9452 13.8463C22.6464 14.5475 23.0452 15.4963 23.0452 16.5C23.0452 17.5038 22.6602 18.4525 21.9452 19.1538L12.9802 28.1188C12.7739 28.3113 12.5127 28.4213 12.2514 28.4213Z' fill='%2380889D'/%3E%3C/svg%3E");
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16' fill='none'%3E%3Cpath d='M5.61035 13.3327L10.0009 8.94211C10.5194 8.42359 10.5194 7.57511 10.0009 7.05659L5.61035 2.66602' stroke='%2380889D' stroke-width='1.5' stroke-miterlimit='10' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/svg%3E");
   background-size: contain;
   background-repeat: no-repeat;
-  opacity: 0.5;
+  flex-shrink: 0;
 }
 
 .breadcrumbs__item-link {
   position: relative;
   display: flex;
   align-items: center;
+  gap: 16px;
   text-decoration: none;
   color: #80889D;
 }
@@ -147,54 +119,32 @@ ol.breadcrumbs {
   display: none;
 }
 
-.breadcrumbs-wrapper {
-  position: relative;
-}
-
-.breadcrumbs {
-  display: flex;
-}
-
-.breadcrumbs--mobile {
-  display: none;
-}
-
 @media (max-width: 1023px) {
-  .breadcrumbs {
+  .breadcrumbs__item {
     display: none;
   }
-
-  ol.breadcrumbs {
-    margin: 0 0 40px;
-  }
   
-  .breadcrumbs--mobile {
+  .breadcrumbs__item:nth-last-child(2) {
     display: flex;
-    margin: 0 0 40px;
-  }
-  
-  .breadcrumbs--mobile .breadcrumbs__item {
-    display: flex;
-  }
-
-  .breadcrumbs__item-link{
-    color: #80889D;
-  }
-  .breadcrumbs--mobile .breadcrumbs__item-link {
-    gap: 16px;
-    color: #80889D;
-    display: flex;
-    align-items: center;
-  }
-  
-  .breadcrumbs--mobile .breadcrumbs__arrow {
     order: -1;
-    transform: rotate(180deg);
-    flex-shrink: 0;
-    fill: #80889D;
-    opacity: 0.5;
+  }
+  
+  .breadcrumbs__item:nth-last-child(2)::after {
+    display: none;
+  }
+  
+  .breadcrumbs__item:nth-last-child(2)::before {
+    content: '';
     width: 16px;
     height: 16px;
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16' fill='none'%3E%3Cpath d='M5.61035 13.3327L10.0009 8.94211C10.5194 8.42359 10.5194 7.57511 10.0009 7.05659L5.61035 2.66602' stroke='%2380889D' stroke-width='1.5' stroke-miterlimit='10' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/svg%3E");
+    background-size: contain;
+    background-repeat: no-repeat;
+    transform: rotate(180deg);
+    flex-shrink: 0;
+    order: -1;
   }
+  
+
 }
 </style>
