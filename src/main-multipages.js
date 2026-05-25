61c8dfcc25e26a332266ad9e93957d39e1dd3523
ze-r0m <rzv2003@gmail.com>
2026-05-25 15:32:32 +0300

    fix: импортирование анимаций для каждой страницы

--- src/main-multipages.js
+++ src/main-multipages.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/styles.css'
+import './assets/animations.css'
+import { initAnimations } from './composables/useAnimations'
 
 const app = createApp(App)
+
+document.addEventListener('DOMContentLoaded', () => {
+    initAnimations()
+})
+
 app.mount('#app')
